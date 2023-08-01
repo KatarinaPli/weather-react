ef3df3eb6f1e7686ba5b2add537c6e2dd7515dec
KatarinaPli <kasiash1010@gmail.com>
2023-08-01 19:15:32 +0200

    Added a form without Bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,35 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather(){
-    
+ let weatherData = {
+   city: "Zurich",
+   temperature: 25,
+   date: "Tuesday 12:00",
+   description: "Cloudy",
+   imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+   humidity: 70,
+   wind: 8,
+ };
+return(
+<div className="Weather">
+    <form className="mb-3">
+        <div className="row">
+            <div className="col-9">
+                <input type="search" placeholder="Type a city..."
+                className="form-control"
+                autoComplete="off"/>
+            </div>
+            <div className="col-3">
+                <input type="submit"
+                value="Search"
+                className="btn btn-primary w-100"/>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+
+)
+
 }
