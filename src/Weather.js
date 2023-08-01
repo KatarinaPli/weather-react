2d3459ab48d4b2312914ab0f236bd82b5b609207
KatarinaPli <kasiash1010@gmail.com>
2023-08-01 19:35:03 +0200

    Added a maine part of weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -11,26 +11,36 @@ export default function Weather(){
    humidity: 70,
    wind: 8,
  };
-return(
-<div className="Weather">
+return (
+  <div className="Weather">
     <form className="mb-3">
-        <div className="row">
-            <div className="col-9">
-                <input type="search" placeholder="Type a city..."
-                className="form-control"
-                autoComplete="off"/>
-            </div>
-            <div className="col-3">
-                <input type="submit"
-                value="Search"
-                className="btn btn-primary w-100"/>
-            </div>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Type a city..."
+            className="form-control"
+            autoComplete="off"
+          />
         </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </div>
     </form>
-</div>
 
-
-
-)
+    <div className="overview">
+      <h1>{weatherData.city}</h1>
+      <ul>
+        <li>Last updated: {weatherData.date}</li>
+        <li>{weatherData.description}</li>
+      </ul>
+    </div>
+  </div>
+);
 
 }
